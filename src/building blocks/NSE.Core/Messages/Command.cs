e6d95868bc50787818cfa364a663c9e70b20492d
lucashfdeus <lucas.hfdeus@gmail.com>
2024-03-26 17:32:53 -0300

    Criando Commands e Events. Add dependência do FluentValidation no Core. Add classes bases do command e message. Add command RegistrarClienteCommand e ClienteCommandHandler.

---
+++ src/building blocks/NSE.Core/Messages/Command.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation.Results;
+using System;
+
+namespace NSE.Core.Messages
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; private set; }
+        public ValidationResult ValidationResult { get; private set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public virtual bool EhValido()
+        {
+            //O virtual utilizado nesse caso é que se quiser usar o 
+            // override é possível porem não é obrigatório.
+            // E se chamar o método sem usar o override irá lançar uma exeção de método não implementado.
+            throw new NotImplementedException();
+        }
+    }
+}
